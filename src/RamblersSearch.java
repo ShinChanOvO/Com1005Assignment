1d97d67f806d4a774af1250b443daed4bc1d5be4
jzw05 <jzw05@172.16.102.68>
2021-05-15 13:48:55 +0800

    some implement about RamblersState and Coords class

--- src/RamblersSearch.java
+++ src/RamblersSearch.java
@@ -1,5 +1,19 @@
 
 
 public class RamblersSearch extends Search {
+	private TerrainMap map; // map we're searching
+	private Coords goal; // goal city
 
-}
+	  public TerrainMap getMap() {
+	    return map;
+	  }
+
+	  public Coords getGoal() {
+	    return goal;
+	  }
+	  //constructor
+	  public RamblersSearch(TerrainMap m, Coords g) {
+	    map = m;
+	    goal = g;
+	  }
+	}
--- src/RamblersState.java
+++ src/RamblersState.java
@@ -2,17 +2,65 @@
 import java.util.ArrayList;
 
 public class RamblersState extends SearchState {
-
+	// coordinate of for this state
+	private Coords coords;
+	  
+	// constructor
+	public RamblersState(Coords inputCoords, int inputLocalCost,int inputEstCost) {
+		coords=inputCoords;
+		localCost = inputLocalCost;
+		estRemCost =inputEstCost;
+	 }  
+	
 	@Override
 	boolean goalPredicate(Search searcher) {
-		// TODO Auto-generated method stub
-		return false;
+		RamblersSearch rsearcher = (RamblersSearch) searcher;
+		Coords tar = rsearcher.getGoal(); // get target coordinate
+		return (coords.equals(tar));
 	}
 
 	@Override
 	ArrayList<SearchState> getSuccessors(Search searcher) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		RamblersSearch rsearcher = (RamblersSearch) searcher;
+	    TerrainMap map = rsearcher.getMap();
+	    int[][]usingArray=map.getTmap();
+	    ArrayList<SearchState> returnArray = new ArrayList<SearchState>();
+	 
+	    //check if the coordinate starts from 0 
+	   if (coords.gety()!=0 && coords.getx()!=0) {
+		   // do the y+ move 
+		   if(usingArray[coords.gety()][coords.getx()] <= usingArray[coords.gety()+1][coords.getx()] ){
+				   int cost=1;
+				   returnArray.add((SearchState) new RamblersState(new Coords(coords.gety()+1,coords.getx()), cost,0));
+				   }
+		   else {
+			   int cost=1+Math.abs((coords.gety()-(coords.gety()+1)));
+			   returnArray.add((SearchState) new RamblersState(new Coords(coords.gety()+1,coords.getx()), cost,0));
+		   		} 
+		   //do the y- move
+		   if(usingArray[coords.gety()][coords.getx()] <= usingArray[coords.gety()-1][coords.getx()] ){
+			   int cost=1;
+			   returnArray.add((SearchState) new RamblersState(new Coords(coords.gety()-1,coords.getx()), cost,0));
+			   }
+		   else {
+			   int cost=1+Math.abs((coords.gety()-(coords.gety()+1)));
+			   returnArray.add((SearchState) new RamblersState(new Coords(coords.gety()-1,coords.getx()), cost,0));
+	   		} 
+		   //do the x+ move
+		   if(usingArray[coords.gety()][coords.getx()] <= usingArray[coords.gety()+1][coords.getx()] ){
+			   int cost=1;
+			   returnArray.add((SearchState) new RamblersState(new Coords(coords.gety()+1,coords.getx()), cost,0));
+			   }
+	   else {
+		   int cost=1+Math.abs((coords.gety()-(coords.gety()+1)));
+		   returnArray.add((SearchState) new RamblersState(new Coords(coords.gety()+1,coords.getx()), cost,0));
+	   		} 
+	     
+	    }
+	    return returnArray;
+	  
+		
 	}
 
 	@Override
